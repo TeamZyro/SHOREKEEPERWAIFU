2ec54a84d441dfbf817f22253bc9d6af4ed4a407
Tᴇᴀᴍ【ＺＹＲＯ】 <harshmanjhi0004@gmail.com>
2025-09-18 11:25:37 +0530

    Update __main__.py

--- TEAMZYRO/__main__.py
+++ TEAMZYRO/__main__.py
@@ -9,9 +9,8 @@ def main() -> None:
         imported_module = importlib.import_module("TEAMZYRO.modules." + module_name)
     LOGGER("TEAMZYRO.modules").info("𝐀𝐥𝐥 𝐅𝐞𝐚𝐭𝐮𝐫𝐞𝐬 𝐋𝐨𝐚𝐝𝐞𝐝 𝐁𝐚𝐛𝐲🥳...")
 
-    ZYRO.run()
-    #application.run_polling(drop_pending_updates=True)
-    #dp.run_polling(bot, drop_pending_updates=True)
+    ZYRO.start()
+    application.run_polling(drop_pending_updates=True)
     LOGGER("TEAMZYRO").info(
         "╔═════ஜ۩۞۩ஜ════╗\n  ☠︎︎MADE BY TEAMZYRO☠︎︎\n╚═════ஜ۩۞۩ஜ════╝"
     )
