4961bf1ef6c41bcef765eda2dd646d1aab29a0f1
Tᴇᴀᴍ【ＺＹＲＯ】 <harshmanjhi0004@gmail.com>
2025-10-29 01:03:40 +0530

    Update __init__.py

--- TEAMZYRO/__init__.py
+++ TEAMZYRO/__init__.py
@@ -28,7 +28,7 @@ def LOGGER(name: str) -> logging.Logger:
 # ---------------------------- CONSTANTS ---------------------------------
 api_id = os.getenv("API_ID", "24965086")
 api_hash = os.getenv("API_HASH", "b9c764ce47c010e1a887f19fea54f648")
-TOKEN = os.getenv("TOKEN", "8482718820:AAEqjcI1vfw-vN23znEj-fHl2yn1UIN7Lyo")
+TOKEN = os.getenv("TOKEN", "8308024939:AAHv49KD7nbKG2HVVCm-9MbBxSSEOXdhSJ0")
 GLOG = os.getenv("GLOG", "Zlog12")
 CHARA_CHANNEL_ID = os.getenv("CHARA_CHANNEL_ID", "Zlog12")
 SUPPORT_CHAT_ID = os.getenv("SUPPORT_CHAT_ID", "-1002056007523")
