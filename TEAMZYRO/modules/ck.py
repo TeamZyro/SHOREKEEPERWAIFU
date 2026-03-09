95a51b1cf4a6b8b1956422d3fdd6361211d98b6b
Tᴇᴀᴍ【ＺＹＲＯ】 <harshmanjhi0004@gmail.com>
2026-03-09 12:34:54 +0530

    Update ck.py

--- TEAMZYRO/modules/ck.py
+++ TEAMZYRO/modules/ck.py
@@ -2,11 +2,12 @@ import asyncio
 import aiohttp
 from pyrogram import filters
 from pyrogram.errors import FloodWait
-from TEAMZYRO import app as ZYRO, collection, OWNER_ID
+from TEAMZYRO import app as ZYRO, collection
+
 
 @ZYRO.on_message(filters.command(["addcheckimg"]))
 async def addcheckimg_command(client, message):
-    if message.from_user.id != OWNER_ID:
+    if message.from_user.id != 7018103039:
         return await message.reply_text("You do not have permission to use this command.")
 
     status_msg = await message.reply_text("Starting image check... This may take a while.")
