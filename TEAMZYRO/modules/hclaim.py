4fbb8b95f17b5f29a2973fd1fe302cd1e32d2ae2
Tᴇᴀᴍ【ＺＹＲＯ】 <harshmanjhi0004@gmail.com>
2026-06-20 08:46:08 +0530

    Update hclaim.py

--- TEAMZYRO/modules/hclaim.py
+++ TEAMZYRO/modules/hclaim.py
@@ -48,7 +48,7 @@ async def mclaim(_, message: t.Message):
     try:
         # Ensure the user is in the correct chat
         if str(message.chat.id) != chat:
-            join_button = InlineKeyboardMarkup([[InlineKeyboardButton("Join Here", url="https://t.me/oneforall_support")]])
+            join_button = InlineKeyboardMarkup([[InlineKeyboardButton("Join Here", url="https://t.me/loverCodesChat")]])
             return await message.reply_text(
                 "🔔 ᴊᴏɪɴ ᴛʜᴇ ᴄʜᴀɴɴᴇʟ ᴛᴏ ᴄʟᴀɪᴍ ʏᴏᴜʀ ᴅᴀɪʟʏ ᴄʜᴀʀᴀᴄᴛᴇʀ 🔔",
                 reply_markup=join_button
